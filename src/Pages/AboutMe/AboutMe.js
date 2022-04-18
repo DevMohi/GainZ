f930ceca790ab5d0178d3767052788b76d6e2587
DevMohi <morphex72@gmail.com>
2022-04-18 19:28:54 +0600

    all mighty push

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -10,8 +10,9 @@ const AboutMe = () => {
         <div className='d-flex justify-content-around container my-5 about-container align-items-center'>
             <div className='w-50 mt-2 about-text'>
                 <div >
-                    <h1 className='display-1'>Hey!Its Me</h1>
+                    <h1 className='display-1'>I Am Mohiuddin</h1>
                     <h1 className='display-3'>Life <span className='custom-clr'>Goals</span></h1>
+
                     <p className='font-weight-bold'>Wanted to take a bit of time of our users, i wanted to talk about my goals in my life. For the past 6 months i have been dedicated in being a web developer, i have worked day and night just to make my skills developed, I hope to continue working like this and be a great web developer in future. I Believe its just a matter of time until i achieve my goals, keep me in your prayers. Thank You For Reading This.</p>
 
                     <div className='icons-container'>
